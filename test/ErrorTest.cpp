946a50c4fe355102156b4abfa8c106eb449388c8
AdamYuan <y13916619121@126.com>
2025-05-01 23:27:07 +0800

    small changes

--- test/ErrorTest.cpp
+++ test/ErrorTest.cpp
@@ -282,7 +282,7 @@ int main(int argc, char **argv) {
 
 			PrintError("RRMSE", rrmse);
 			for (uint32_t k = 0; k < kMRECount; ++k)
-				PrintError((std::string("avg ") + mreFuncs[k].GetPrefix()).c_str(), mres[k]);
+				PrintError(mreFuncs[k].GetPrefix().c_str(), mres[k]);
 
 			++sumCount;
 			sumRRMSE += rrmse;
